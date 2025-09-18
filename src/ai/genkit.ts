8e78595a6975686c67478b54eab5d2eb084cf209
Ratheesh kanth Ravichandran <ratheeshkanthravichandran@gmail.com>
2025-09-18 21:54:37 +0000

    same

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({tools: [googleAI.googleSearch]})],
+  plugins: [googleAI()],
   model: 'googleai/gemini-2.5-flash',
 });
